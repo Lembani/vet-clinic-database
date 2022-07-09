41f856640f956fc170448261fa72eb9d779bd312
Lembani Sakala <lembanisakala@gmail.com>
2022-07-09 05:23:11 +0200

    Add sample data to DB.

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO ANIMALS(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(1, 'Agumon', 'Feb 3 2020', 0, true, 10.23);
+
+INSERT INTO ANIMALS(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(2, 'Gabumon', 'Nov 15 2018', 2, true, 8);
+
+ INSERT INTO ANIMALS(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(3, 'Pikachu', 'Jan 7 2021', 1, false, 15.04);
+
+ INSERT INTO ANIMALS(id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES(4, 'Devimon', 'May 12 2017', 5, true, 11);
+ 
