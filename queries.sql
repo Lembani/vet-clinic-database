e25717110d73f11d405f97246b0f1a70c08a0f99
Lembani Sakala <lembanisakala@gmail.com>
2022-07-09 08:14:24 +0200

    Add Aggregates: - Total Animals - Animals which never tried to escape - Average weight of animals - Which animal escapes the most, neutered or not neutered animals - Minimum and maximum weight of each type of animal - Average number of escape attempts per animal type of those born between 1990 and 2000

--- queries.sql
+++ queries.sql
@@ -40,3 +40,23 @@ UPDATE ANIMALS SET weight_kg = weight_kg * (-1);
 ROLLBACK TO savepoint;
 UPDATE ANIMALS SET weight_kg = weight_kg * (-1) WHERE weight_kg < 0;
 COMMIT;
+
+/*AGGREGATES*/
+
+/*Total Animals*/
+SELECT COUNT(*) FROM ANIMALS;
+
+/*Animals which never tried to escape*/
+SELECT COUNT(*) FROM ANIMALS WHERE escape_attempts = 0;
+
+/*Average weight of animals*/
+SELECT AVG(weight_kg) FROM ANIMALS;
+
+/*Which animal escapes the most, neutered or not neutered animals*/
+SELECT neutered, MAX(escape_attempts) FROM ANIMALS GROUP BY neutered;
+
+/*Minimum and maximum weight of each type of animal*/
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM ANIMALS GROUP BY species;
+
+/*Average number of escape attempts per animal type of those born between 1990 and 2000*/
+SELECT species, AVG(escape_attempts) FROM ANIMALS WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
