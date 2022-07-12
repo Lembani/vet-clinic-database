7bc07bf563ff2752c9835be58b24730d4176a5b7
onomeh <onwuanibeonome@gmail.com>
2022-07-12 15:53:42 +0000

    Create invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,6 +1,15 @@
 
 CREATE TABLE patients (
-  id SERIAL PRIMARY KEY,
+  id INT SERIAL PRIMARY KEY,
   name VARCHAR(200),
   date_of_birth date
+);
+
+CREATE TABLE invoices (
+  id  INT SERIAL PRIMARY KEY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT,
+  CONSTRAINT fk_medical_histories FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 );
