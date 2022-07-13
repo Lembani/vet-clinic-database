52b315d019f5f560a3272a653752fcec2cf1bc10
Lembani Sakala <lembanisakala@gmail.com>
2022-07-13 03:48:35 +0200

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,10 +6,18 @@ CREATE TABLE patients (
 );
 
 CREATE TABLE invoices (
-  id  INT SERIAL PRIMARY KEY,
+  id INT SERIAL PRIMARY KEY,
   total_amount DECIMAL,
   generated_at TIMESTAMP,
   payed_at TIMESTAMP,
   medical_history_id INT,
   CONSTRAINT fk_medical_histories FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE medical_histories (
+  id INT SERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR(200),
+  CONSTRAINT fk_patients FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
