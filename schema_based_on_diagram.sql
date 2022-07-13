efe3eff438a1782cf5d14e53d6515ea68b2a150f
Lembani Sakala <lembanisakala@gmail.com>
2022-07-13 10:09:57 +0200

    Add invoices medical treatments treatment Id index

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -62,3 +62,5 @@ CREATE INDEX idx_invoice_items_invoice_id ON invoice_items(invoice_id);
 
 CREATE INDEX idx_invoice_items_treatment_id ON invoice_items(treatment_id);
 
+CREATE INDEX idx_medical_treatments_treatment_id ON medical_treatments(treatment_id);
+
